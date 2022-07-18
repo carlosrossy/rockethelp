3163ba4c8a018795f9eea578b037bf884ae31806
CarlosBitizin <carlospintorossy07@gmail.com>
2022-07-18 19:55:10 -0400

    theme creation and logo addition

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { NativeBaseProvider } from 'native-base'
+
+import { THEME } from './src/styles/theme';
+
 import { SingIn } from "./src/screens/SingIn";
 
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <SingIn />
     </NativeBaseProvider>
   );
