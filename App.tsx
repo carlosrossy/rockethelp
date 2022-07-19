427db40de482105066759b235decb54534917592
CarlosBitizin <carlospintorossy07@gmail.com>
2022-07-18 20:14:42 -0400

    font installation and adding font loading

--- App.tsx
+++ App.tsx
@@ -1,13 +1,24 @@
 import { NativeBaseProvider } from 'native-base'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
 
 import { THEME } from './src/styles/theme';
 
 import { SingIn } from "./src/screens/SingIn";
+import { Loading } from './src/screens/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <SingIn />
+      {fontsLoaded ? <SingIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
