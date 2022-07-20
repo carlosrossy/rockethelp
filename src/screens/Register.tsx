935c970fee2d17a42ba7afa9c82eab588a85da32
CarlosBitizin <carlospintorossy07@gmail.com>
2022-07-19 22:53:56 -0400

    register interface creation

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,11 +1,31 @@
 import { VStack } from 'native-base';
 
 import { Header } from '../components/Header';
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
 
 export function Register() {
     return (
-        <VStack flex={1} pb={6} bg="gray.600">
+        <VStack flex={1} p={6} bg="gray.600">
             <Header title='Nova Solicitação' />
+
+            <Input
+                placeholder='Número do Patrimônio'
+                mt={4}
+            />
+
+            <Input
+                placeholder='Descrição do problema'
+                flex={1}
+                multiline
+                textAlignVertical='top'
+                mt={5}
+            />
+
+            <Button
+                title='Enviar'
+                mt={5}
+            />
         </VStack>
     );
 }
