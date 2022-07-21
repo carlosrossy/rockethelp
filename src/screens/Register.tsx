51322f3406c36a5af136b9c25161799a3e61b303
CarlosBitizin <carlospintorossy07@gmail.com>
2022-07-20 23:42:47 -0400

    implementation of logging requests

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,10 +1,47 @@
+import { useState } from 'react';
+import { Alert } from 'react-native';
 import { VStack } from 'native-base';
+import { useNavigation } from '@react-navigation/native'
+import fireStore from '@react-native-firebase/firestore'
 
 import { Header } from '../components/Header';
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
+
 export function Register() {
+    const [isLoading, setLoading] = useState(false);
+    const [patrimony, setPatrimony] = useState('');
+    const [description, setDescription] = useState('');
+
+    const navigation = useNavigation();
+
+    function handleNewOrderRegister() {
+        if (!patrimony || !description) {
+            return Alert.alert("Registrar", "Preencha todos os campos");
+        }
+
+        setLoading(true);
+
+        fireStore()
+            .collection('orders')
+            .add({
+                patrimony,
+                description,
+                status: 'open',
+                created_at: fireStore.FieldValue.serverTimestamp()
+            })
+            .then(() => {
+                Alert.alert("Solicitação", "Solicitação resgitrada com sucesso");
+                navigation.goBack();
+            })
+            .catch((error) => {
+                console.log(error);
+                setLoading(false);
+                return Alert.alert("Solicitação", "Não foi possivel registrar o pedido");
+            })
+    }
+
     return (
         <VStack flex={1} p={6} bg="gray.600">
             <Header title='Nova Solicitação' />
@@ -12,6 +49,7 @@ export function Register() {
             <Input
                 placeholder='Número do Patrimônio'
                 mt={4}
+                onChangeText={setPatrimony}
             />
 
             <Input
@@ -20,11 +58,14 @@ export function Register() {
                 multiline
                 textAlignVertical='top'
                 mt={5}
+                onChangeText={setDescription}
             />
 
             <Button
                 title='Enviar'
                 mt={5}
+                isLoading={isLoading}
+                onPress={handleNewOrderRegister}
             />
         </VStack>
     );
