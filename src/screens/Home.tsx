733e07d65357ab4f1ddd51e8a2802ac1a579bb62
CarlosBitizin <carlospintorossy07@gmail.com>
2022-07-21 00:04:11 -0400

    showing requests stored in the bank

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,39 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Alert } from 'react-native';
 import auth from '@react-native-firebase/auth'
+import fireStore from '@react-native-firebase/firestore'
 import { useNavigation } from '@react-navigation/native';
 import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList, Center } from 'native-base';
 import { SignOut, ChatTeardropText } from "phosphor-react-native"
 
+import { dateFormat } from "../utils/firestoreDateFormats"
 import Logo from '../assets/logo_secondary.svg'
 
 import { Filter } from '../components/Filter';
 import { Button } from '../components/Button';
+import { Loading } from '../components/Loading';
 import { Order, OrderProps } from '../components/Order';
 
-export function Home() {
 
+export function Home() {
+    const [isLoading, setLoading] = useState(true);
     const [statusSelected, setstatusSelected] = useState<'open' | 'closed'>('open')
-    const [orders, setOrders] = useState<OrderProps[]>([
-        {
-            id: "123",
-            patrimony: "123456",
-            when: "18/07/2022 as 14:00",
-            status: 'open'
-        },
-        {
-            id: "1234",
-            patrimony: "242424",
-            when: "19/07/2022 as 15:00",
-            status: 'open'
-        },
-        {
-            id: "12345",
-            patrimony: "565342",
-            when: "20/07/2022 as 16:00",
-            status: 'open'
-        }
-    ]);
+    const [orders, setOrders] = useState<OrderProps[]>([]);
 
     const navigation = useNavigation();
     const { colors } = useTheme();
@@ -54,6 +39,31 @@ export function Home() {
                 return Alert.alert("Sair", "Não foi possivel sair.")
             })
     }
+
+    useEffect(() => {
+        setLoading(true);
+
+        const subscriber = fireStore()
+            .collection("orders")
+            .where('status', '==', statusSelected)
+            .onSnapshot(snapshot => {
+                const data = snapshot.docs.map(doc => {
+                    const { patrimony, description, status, created_at } = doc.data();
+
+                    return {
+                        id: doc.id,
+                        patrimony,
+                        description,
+                        status,
+                        when: dateFormat(created_at)
+                    }
+                });
+                setOrders(data);
+                setLoading(false);
+            });
+
+        return subscriber;
+    }, [statusSelected])
 
     return (
         <VStack flex={1} pb={6} bg="gray.700">
@@ -101,23 +111,26 @@ export function Home() {
                     />
 
                 </HStack>
+                {
+                    isLoading ? <Loading /> :
+                        <FlatList
+                            data={orders}
+                            keyExtractor={item => item.id}
+                            renderItem={({ item }) => <Order data={item} onPress={() => handleOpenDetails(item.id)} />}
+                            showsVerticalScrollIndicator={false}
+                            contentContainerStyle={{ paddingBottom: 100 }}
+                            ListEmptyComponent={() => (
+                                <Center>
+                                    <ChatTeardropText color={colors.gray[300]} size={40} />
+                                    <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                                        Você ainda não possui {"\n"}
+                                        solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
+                                    </Text>
+                                </Center>
+                            )}
+                        />
+                }
 
-                <FlatList
-                    data={orders}
-                    keyExtractor={item => item.id}
-                    renderItem={({ item }) => <Order data={item} onPress={() => handleOpenDetails(item.id)} />}
-                    showsVerticalScrollIndicator={false}
-                    contentContainerStyle={{ paddingBottom: 100 }}
-                    ListEmptyComponent={() => (
-                        <Center>
-                            <ChatTeardropText color={colors.gray[300]} size={40} />
-                            <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
-                                Você ainda não possui {"\n"}
-                                solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
-                            </Text>
-                        </Center>
-                    )}
-                />
 
                 <Button title='Nova solicitação' onPress={handleNewOrder} />
             </VStack>
