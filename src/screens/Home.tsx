d41782650e2f9f63b7fa6f701b12768773a8ef0c
CarlosBitizin <carlospintorossy07@gmail.com>
2022-07-20 22:36:16 -0400

    configuring Firebase Auth and Cloud Firestore project

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -65,11 +65,11 @@ export function Home() {
             <VStack flex={1} px={6}>
                 <HStack w="full" mt={8} mb={6} justifyContent="space-between" alignItems="center">
                     <Heading color="gray.100">
-                        Meus Chamados
+                        Solicitações
                     </Heading>
 
                     <Text color="gray.200">
-                        3
+                        {orders.length}
                     </Text>
                 </HStack>
 
